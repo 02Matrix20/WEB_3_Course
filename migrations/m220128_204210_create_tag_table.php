bc0e9debe7d4d460c5d15794df935346b5bf3d35
02Matrix20 <65680882+02Matrix20@users.noreply.github.com>
2022-02-19 16:13:15 +0200

    Lab_1-5

---
+++ migrations/m220128_204210_create_tag_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%tag}}`.
+ */
+class m220128_204210_create_tag_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+                $this->createTable('tag', [
+            'id' => $this->primaryKey(),
+            'title'=>$this->string(),
+        ]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%tag}}');
+    }
+}
